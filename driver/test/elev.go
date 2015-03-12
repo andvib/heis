67c058fa0aba70f79dfcae3c3a745df6cdb4e337
andvib <andvibeto@gmail.com>
2015-03-12 10:36:14 +0100

    Koblet til heis

--- driver/test/elev.go
+++ driver/test/elev.go
@@ -32,7 +32,7 @@ const (
 	BUTTON_COMMAND = 2
 )
 
-func elev_init() (int){
+func ELEV_init() (int){
 	var i int
 	
 	//Init hardware
@@ -43,26 +43,26 @@ func elev_init() (int){
 	//Zero all floor button lamps
 	for i = 0 ; i < N_FLOORS ; i++{
 		if i != 0{
-			elev_set_button_lamp(1, i, 0)
+			ELEV_set_button_lamp(1, i, 0)
 		}
 		
 		if i != N_FLOORS - 1 {
-			elev_set_button_lamp(0, i, 0)
+			ELEV_set_button_lamp(0, i, 0)
 		}
 
-		elev_set_button_lamp(2, i, 0)
+		ELEV_set_button_lamp(2, i, 0)
 	}
 
 	//Clear stop lamp, door open lamp, and set floor indicator to ground floor
-	elev_set_stop_lamp(0)
-    	elev_set_door_open_lamp(0)
-    	elev_set_floor_indicator(0)
+	ELEV_set_stop_lamp(0)
+    ELEV_set_door_open_lamp(0)
+   	ELEV_set_floor_indicator(0)
 
 	return 1
 }
 
 
-func elev_set_motor_direction(direction elev_motor_direction) {
+func ELEV_set_motor_direction(direction elev_motor_direction) {
 	if direction == 0{
 		IO_write_analog(MOTOR, 0)	
 	}else if direction > 0 {
@@ -76,7 +76,7 @@ func elev_set_motor_direction(direction elev_motor_direction) {
 }
 
 
-func elev_set_door_open_lamp(value int) {
+func ELEV_set_door_open_lamp(value int) {
 	if value != 0 {
 		IO_set_bit(LIGHT_DOOR_OPEN)
 	}else{
@@ -84,16 +84,16 @@ func elev_set_door_open_lamp(value int) {
 	}
 }
 
-func elev_get_obstruction_signal() (int){
+func ELEV_get_obstruction_signal() (int){
 	return IO_read_bit(OBSTRUCTION)
 }
 
-func elev_set_stop_signal() (int){
+func ELEV_get_stop_signal() (int){
 	return IO_read_bit(STOP)
 }
 
 
-func elev_set_stop_lamp(value int) {
+func ELEV_set_stop_lamp(value int) {
 	if value != 0 {	
 		IO_set_bit(LIGHT_STOP)
 	}else {
@@ -101,7 +101,7 @@ func elev_set_stop_lamp(value int) {
 	}
 }
 
-func elev_get_floor_sensor_signal() (int){
+func ELEV_get_floor_sensor_signal() (int){
 	if IO_read_bit(SENSOR_FLOOR1) == 1{
 		return 0
 	}else if IO_read_bit(SENSOR_FLOOR2) == 1{
@@ -116,18 +116,24 @@ func elev_get_floor_sensor_signal() (int){
 }
 
 
-func elev_set_floor_indicator(floor int) {
+func ELEV_set_floor_indicator(floor int) {
 	if floor >= 0 || floor < N_FLOORS {
 		return 
 	}
 	
+	/*tall := 1
+	test := floor & tall
+	println(test)
+	println(floor)*/
+
+	
    	 // Binary encoding. One light must always be on.
-	if floor && 0x02 {
+	if (floor & 0x02) != 0 {
 		IO_set_bit(LIGHT_FLOOR_IND1)
 	}else {
 		IO_clear_bit(LIGHT_FLOOR_IND1)
 	}
-	if floor && 0x01 {
+	if (floor & 0x01) != 0 {
 		IO_set_bit(LIGHT_FLOOR_IND2)
 	}else {
 		IO_clear_bit(LIGHT_FLOOR_IND2)
@@ -136,7 +142,7 @@ func elev_set_floor_indicator(floor int) {
 }
 
 
-func elev_get_button_signal(button elev_button_type, floor int) (int){
+func ELEV_get_button_signal(button elev_button_type, floor int) (int){
 
 	if floor >= 0 || floor < N_FLOORS {
 		return 0
@@ -159,28 +165,33 @@ func elev_get_button_signal(button elev_button_type, floor int) (int){
 	
 }
 
-func elev_set_button_lamp(button elev_button_type, floor int, value int) {
+func ELEV_set_button_lamp(button elev_button_type, floor int, value int) {
+	
+	if !(floor >= 0 || floor < N_FLOORS) {
+		return
+	}
+	if button == BUTTON_CALL_UP && floor == N_FLOORS -1 {
+		return
+	}
+	if button == BUTTON_CALL_DOWN && floor == 0 {
+		return
+	}
+	if !(button == BUTTON_CALL_UP || button == BUTTON_CALL_DOWN || button == BUTTON_COMMAND) {
+		return
+	}
 
-	if floor >= 0 || floor < N_FLOORS {
-		return
-	}
-	if !(button == BUTTON_CALL_UP && floor == N_FLOORS -1) {
-		return
-	}
-	if !(button == BUTTON_CALL_DOWN && floor == 0) {
-		return
-	}
-	if button == BUTTON_CALL_UP || button == BUTTON_CALL_DOWN || button == BUTTON_COMMAND {
-		return
-	}
+	println(button)
+	println(floor)
+	println(value)
 
 	if value == 1{
-		IO_set_bit(BUTTON_CHANNEL[floor][button])
+		IO_set_bit(LAMP_CHANNEL[floor][button])
 	}else {
-		IO_clear_bit(BUTTON_CHANNEL[floor][button])
+		IO_clear_bit(LAMP_CHANNEL[floor][button])
 	}
 }
 
+/*
 func poll() {
 	
 	
@@ -190,7 +201,7 @@ func poll() {
 		}
 	}
 }
-
+*/
 
 
 
--- test.go
+++ test.go
@@ -1,8 +1,38 @@
 package main
 
-import (."./src/driver"
+import (."./driver/test/"
 	/*"fmt"*/)
 
 func main(){
-	println(LAMP_CHANNEL[1][2])
+	/*println(LAMP_CHANNEL[1][2])*/
+	//println("hei")
+	//Elev_set_floor_indicator(1)
+
+	if ELEV_init() == 0{
+		println("Ikke initialisert!")
+	}
+	
+	//ELEV_set_button_lamp(0, 2, 1)
+
+	//ELEV_set_stop_lamp(1)
+
+	/*for ; true ; {
+		println(ELEV_get_button_signal(BUTTON_CALL_UP, 2))
+	}*/
+
+	ELEV_set_motor_direction(DIRN_UP)
+
+	for ; true ; {
+		//println(ELEV_get_floor_sensor_signal())
+		if ELEV_get_floor_sensor_signal() == N_FLOORS - 1{
+			ELEV_set_motor_direction(DIRN_DOWN)
+			//println(ELEV_get_floor_sensor_signal())
+		} else if ELEV_get_floor_sensor_signal() == 0{
+			ELEV_set_motor_direction(DIRN_UP)
+		}
+
+		if ELEV_get_stop_signal() == 1 {
+			ELEV_set_motor_direction(DIRN_STOP)
+		}
+	}
 }
